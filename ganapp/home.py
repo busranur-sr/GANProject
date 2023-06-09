bea38aec8b2e9472c9746e23b4b22e57e7982784
büşra\nur <70486898+busranur-sr@users.noreply.github.com>
2023-06-09 13:55:21 +0300

    Add files via upload

--- ganapp/home.py
+++ ganapp/home.py
@@ -9,25 +9,35 @@ from django.shortcuts import render
 import tempfile
 from django.shortcuts import redirect
 from django.urls import reverse
-
+from django.contrib import messages
+from ganapp.superresolution import upscaleImage
 
 class ImageUploadForm(forms.Form):
-    selectOption = forms.ChoiceField(choices=[('cartoon', 'CartoonGan'),
-                                            ('monet', 'CycleGan - Monet'),
-                                            ('vangogh', 'CycleGan - Vangogh'),
-                                            ('cezanne', 'CycleGan - Cezanne'),
-                                            ('ukiyoe', 'CycleGan - Ukiyoe')],
-                                   widget=forms.Select(attrs={'class': 'style-select'}),
-                                   required=True)
+    selectOption = forms.ChoiceField(
+        choices=[
+            ("cartoon", "CartoonGan"),
+            ("monet", "CycleGan - Monet"),
+            ("vangogh", "CycleGan - Vangogh"),
+            ("cezanne", "CycleGan - Cezanne"),
+            ("ukiyoe", "CycleGan - Ukiyoe"),
+        ],
+        widget=forms.Select(attrs={"class": "style-select"}),
+        required=True,
+    )
 
     image = forms.ImageField(label="Upload Image")
     # Diğer alanlar
 
+
 def getModel(style):
-    if(style == "cartoon"):
+    if style == "cartoon":
         return Cartoonize()
     else:
         return CycleGANGenerator(style)
+
+
+from django.http import JsonResponse
+
 
 @login_required
 def home_view(request):
@@ -35,29 +45,29 @@ def home_view(request):
         form = ImageUploadForm(request.POST, request.FILES)
         if form.is_valid():
             style = form.cleaned_data["selectOption"]
-            cartoonizer = getModel(style) 
+            cartoonizer = getModel(style)
             with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as temp:
                 temp.write(request.FILES["image"].read())
                 cartoon_image = cartoonizer.forward(temp.name)
+                upscaleImage(cartoon_image)
                 # Save the original image and the cartoonized image to the database
                 user_image = UserImage(
                     user=request.user,
                     image=request.FILES["image"],
                     output_image=cartoon_image,
+                    style=style,
                 )
                 user_image.save()
                 # Add delay to simulate AI processing time
                 time.sleep(5)  # Adjust the delay duration as needed
-                print("yes")
-                return redirect("result")    
+                return JsonResponse({"success": True})
         else:
-            print(form.errors.as_data())
-    
+            return JsonResponse({"success": False, "errors": form.errors.as_json()})
     else:
         form = ImageUploadForm()
-    print("no")
-    user_images = UserImage.objects.filter(user=request.user).last()
-    return render(request, "home.html", {"form": form, "user_images": user_images})
+        user_images = UserImage.objects.filter(user=request.user).last()
+        return render(request, "home.html", {"form": form, "user_images": user_images})
+
 
 @login_required
 def result_view(request):
