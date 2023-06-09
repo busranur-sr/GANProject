71dd95c5a6051e71f537a72dcb83f00b49273304
büşra\nur <70486898+busranur-sr@users.noreply.github.com>
2023-06-09 10:34:43 +0300

    Add files via upload

--- ganapp/models.py
+++ ganapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
 
+
 class Photo(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to="photos/")
@@ -11,13 +12,23 @@ class Photo(models.Model):
 class GeneratedImage(models.Model):
     input_image = models.ImageField(upload_to="input_images/")
     output_image = models.ImageField(upload_to="output_images/")
-
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"GeneratedImage {self.id}"
 
+
 class UserImage(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='original_images/')
-    output_image = models.ImageField(upload_to='output_images/', null=True, blank=True)
+    image = models.ImageField(upload_to="original_images/")
+    output_image = models.ImageField(upload_to="output_images/", null=True, blank=True)
+    style = models.CharField(max_length=20, default="Unknown")
+
+
+from django.contrib.auth import get_user_model
+
+
+class Suggestion(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
